07c94e0315e5c247f59b0dc5ee3e3834bb3c19dd
VladDadonkin <vlad.dadonkin.02@mail.ru>
2025-02-16 18:08:06 +0300

    run 1.0

---
+++ src/js/app.js
@@ -0,0 +1,29 @@
+const gameField = document.querySelector(".gameField");
+const allCells = 16;
+
+for (let i = 0; i < allCells; i++) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+  gameField.appendChild(cell);
+  cell.dataset.index = i;
+}
+
+const goblin = document.createElement("img");
+goblin.src = "./img/goblin.png";
+goblin.alt = "goblin";
+
+let currentCellIndex = Math.floor(Math.random() * allCells);
+const cells = document.querySelectorAll(".cell");
+cells[currentCellIndex].appendChild(goblin);
+
+function moveCharacter() {
+  let newIndex;
+  do {
+    newIndex = Math.floor(Math.random() * allCells);
+  } while (newIndex === currentCellIndex);
+
+  cells[newIndex].appendChild(goblin);
+  currentCellIndex = newIndex;
+}
+
+setInterval(moveCharacter, 2000);
